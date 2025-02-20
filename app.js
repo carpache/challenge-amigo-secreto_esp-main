affc64ea8a443cdd0442f9594e72a7c0769e2db8
Carlos Pacheco <hernandez474@gmail.com>
2025-02-20 17:41:01 -0500

    Actialice js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+console.log(amigos);
+
+function agregarAmigo (){
+    let amigosIngresados = document.getElementById ("amigo").value;
+    
+    if(amigosIngresados === "" ){
+        alert ('por favor, inserte un nombre');
+
+     } else{amigos.push(amigosIngresados);
+
+        document.getElementById("amigo").value = "";
+
+        listaDeAmigos();
+        actualizarLista();
+
+        return;
+
+     }
+
+
+
+
+
+
+}
